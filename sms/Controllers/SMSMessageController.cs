6b8d92158f788da584fc7885713b4760be7d354c
dhanpalan.naidu <dhanpalan.naidu@TERENCENAIDU>
2018-09-20 18:10:33 +0200

    Add project files.

---
+++ sms/Controllers/SMSMessageController.cs
@@ -0,0 +1,39 @@
+﻿using Nexmo.Api;
+using sms.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace sms.Controllers
+{
+    public class SMSMessageController : Controller
+    {
+        // GET: SMSMessage
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMessage(Message message)
+        {
+            var results = SMS.Send(new SMS.SMSRequest
+            {
+                from = Configuration.Instance.Settings["appsettings:27815833532"],
+                to = message.To,
+                text = message.ContentMsg
+            });
+
+
+            return View();
+        }
+    }
+}
